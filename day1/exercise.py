df8ebd0498461d9da548575dd103f393442e8b1c
unknown <ubiiwali@gmail.com>
2022-10-07 17:51:22 +0300

    first commit

---
+++ day1/exercise.py
@@ -0,0 +1,40 @@
+# 1. Check the python version you are using
+''' python --version '''
+# 2. Open the python interactive shell and do the following operations. The operands are 3 and 4.
+print(3 + 4)              #addition(+)
+print(3 - 4)              #subtraction(-)
+print(3 * 4)              #multiplication(*)
+print(3 % 4)              #modulus(%)
+print(3 / 4)              #division(/)
+print(3 ** 4)             #exponential(**)
+print(3 // 4)             #floor division operator(//)
+
+# 3. Write strings on the python interactive shell. The strings are the following:
+First_Name =  "Sharon"         #Your name
+Last_Name  =  "Fabian"         #Your family name
+Country    =  "Nigeria"        # Your country
+"I am enjoying 30 days of python"
+# 4. Check the data types of the following data:
+print(type(10))  
+print(type(9.8)) 
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type(First_Name))
+print(type(Last_Name))
+print(type(Country))
+
+ #Exercise: Level 2
+# 5. Create a folder named day_1 inside 30DaysOfPython folder. Inside day_1 folder, create a python file helloworld.py and repeat questions 1, 2, 3 and 4. Remember to use print() when you are working on a python file. Navigate to the directory where you have saved your file, and run it.
+ #Exercise: Level 3
+# 6. Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+print(20)                                          #Integer
+print(2.4)                                         #Float
+# 5 + j                                            #Complex
+print('Nigeria')                                   #String
+print(['Nigeria', 'Estonia', 'Somalia','Ghana'])   #List
+print(('Blessing', 'Sophie', 'Faith', 'Sharon'))   #Tuple
+print({3, 5, 7, 2, 9, 1})                          #Set
+print({'City':'Abuja'})                            #Dictionary
+
+# 7.Find an Euclidian distance between (2, 3) and (10, 8)
